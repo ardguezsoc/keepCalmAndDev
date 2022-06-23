4aa89e01c70334ce4588a846f8f125a1ee29e4ad
Andrea <andreac.3108@gmail.com>
2022-06-23 18:20:58 +0200

    feat: add background videos

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,6 +1,10 @@
-import { MainButtonsContainer, StyledHomePage } from 'pages/HomePage/HomePage.styled'
-import { useTips } from 'hooks'
-import { Button } from 'components/Button/Button'
+import backgroundVideo from 'assets/videos/blue-ocean.mp4';
+import { Button } from 'components/Button/Button';
+import { useTips } from 'hooks';
+import {
+  BackgroundVideo, MainButtonsContainer,
+  StyledHomePage
+} from 'pages/HomePage/HomePage.styled';
 
 const HomePage = () => {
   const { getTipsPage } = useTips();
@@ -12,16 +16,20 @@ const HomePage = () => {
       <p>{description}</p>
       <p>{id}</p>
       <MainButtonsContainer>
-        <Button buttonIconName='eyes'>
-          Find your best calm tips
-        </Button>
-        <Button buttonIconName='eyes'>
-          Relax with some music
-        </Button>
-        <Button buttonIconName='eyes'>
-          Find your path to peace
-        </Button>
+        <Button buttonIconName="eyes">Find your best calm tips</Button>
+        <Button buttonIconName="eyes">Relax with some music</Button>
+        <Button buttonIconName="eyes">Find your path to peace</Button>
       </MainButtonsContainer>
+
+      <BackgroundVideo
+        height={1080}
+        width={1920}
+        loop={true}
+        autoPlay="autoplay"
+        muted
+      >
+        <source src={backgroundVideo} type="video/mp4" />
+      </BackgroundVideo>
     </StyledHomePage>
   );
 };
--- src/pages/HomePage/HomePage.styled.js
+++ src/pages/HomePage/HomePage.styled.js
@@ -1,31 +1,61 @@
 import styled from 'styled-components/macro';
-import background from 'assets/images/background.jpg';
 
 export const StyledHomePage = styled.div`
   border: none;
   padding: 1rem 2rem;
   height: 100vh;
   position: relative;
-  background-image: url(${background});
   background-size: 100% 100%;
-   width: 100vw;
+  width: 100vw;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  top: 0;
+
   .mainTitle {
     font-size: 1.9rem;
     text-align: center;
   }
-`;
+  `
+
+export const BackgroundVideo = styled.video`
+  height: 56.25vw; // for a 16:9 aspect ratio, 9/16*100 = 56.25
+  left: 50%;
+  min-height: 100vh;
+  min-width: 177.77vh; // for a 16:9 aspect ratio, 16/9*100 = 177.77
+  position: absolute;
+  top: 50%;
+  transform: translate(-50%, -50%);
+  width: 100vw;
+  z-index: -1;
+`
+
+export const Overlay = styled.div`
+  background-color: rgba(0, 0, 0, 0.66);
+  bottom: 0;
+  box-shadow: inset 0 0 5rem rgba(0, 0, 0, 0.5);
+  left: 0;
+  min-height: 100%;
+  min-width: 100%;
+  object-fit: cover;
+  object-position: center;
+  position: fixed;
+  right: 0;
+  top: 0;
+  z-index: -1;
+`
 
 export const MainButtonsContainer = styled.div`
   width: 400px;
   display: grid;
   flex-direction: row;
   grid-template-columns: 1fr;
-  gap:1rem
+  gap: 1rem
 `
 export const StyledTitle = styled.div`
   margin: 2rem 0;
-  font-size: ${({ theme }) => theme.font.size.normal};
-  border: 2px solid ${({ theme }) => theme.palette.common.greenSuccess};
+  font-size: ${({theme}) => theme.font.size.normal};
+  border: 2px solid ${({theme}) => theme.palette.common.greenSuccess};
   position: absolute;
   left: -10rem;
   top: 9.42rem;
@@ -41,8 +71,8 @@ export const StyledAddButton = styled.button`
   right: 3%;
   bottom: 3%;
   border-radius: 50%;
-  background-color: ${({ theme }) => theme.palette.common.greenSuccess};
-  color: ${({ theme }) => theme.palette.common.white};
+  background-color: ${({theme}) => theme.palette.common.greenSuccess};
+  color: ${({theme}) => theme.palette.common.white};
   font-size: 2rem;
   display: flex;
   justify-content: center;
