475a2e2d3325aa488714aded0d50f4faa7c6ec8d
Adrian <adrian.rodriguez@guidesmiths.com>
2022-06-28 19:56:13 +0200

    feat: adding mocked data

---
+++ src/repositories/mockedDataTips.js
@@ -0,0 +1,28 @@
+ const data = [
+    {
+      quote: 'Calmness of mind is one of the beautiful jewels of wisdom',
+      author: 'James Allen',
+    },
+    {
+      quote:
+        'Nothing ever goes away until it has taught us what we need to know',
+      author: 'Pema Chodron',
+    },
+    {
+      quote:
+        'The feeling that any task is a nuisance will soon disappear if it is done in mindfulness',
+      author: 'Thích Nhất Hạnh',
+    },
+    {
+      quote:
+        'Mindfulness is a way of befriending ourselves and our experience',
+      author: 'Jon Kabat-Zinn',
+    },
+    {
+      quote:
+        'Learn to slow down. Get lost intentionally. Observe how you judge both yourself and those around you',
+      author: 'Tim Ferriss',
+    },
+  ];
+
+  export default data;
--- src/repositories/tips.jsx
+++ src/repositories/tips.jsx
@@ -1,12 +1,9 @@
 import axios from 'axios';
+import dataTipsMocked from './mockedDataTips';
 
 export const createTipRepository = () => {
   const getTipsPage = async () => {
-    const { data } = await axios.get(
-      `https://860b-80-26-195-161.eu.ngrok.io/Quote`,
-    );
-
-    return data;
+    return dataTipsMocked;
   };
 
   const postTip = async (id) => {
